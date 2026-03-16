19d9b9edc5c53e830025ae87e7d52cbed55ed9fa
EstebanBarrero <barrerodavid10@gmail.com>
2026-03-15 21:09:27 -0500

    fix: add missing migrations

--- autoctonos/settings.py
+++ autoctonos/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -84,20 +85,10 @@ WSGI_APPLICATION = 'autoctonos.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-import dj_database_url
 
 DATABASES = {
     'default': dj_database_url.config(
-        default={
-            'ENGINE': 'django.db.backends.{}'.format(
-                os.getenv('DATABASE_ENGINE', 'sqlite3')
-            ),
-            'NAME': os.getenv('DATABASE_NAME', 'polls'),
-            'USER': os.getenv('DATABASE_USERNAME', 'myprojectuser'),
-            'PASSWORD': os.getenv('DATABASE_PASSWORD', 'password'),
-            'HOST': os.getenv('DATABASE_HOST', '127.0.0.1'),
-            'PORT': os.getenv('DATABASE_PORT', 5432),
-        },
+        default=f"postgresql://{os.getenv('DATABASE_USERNAME', 'myprojectuser')}:{os.getenv('DATABASE_PASSWORD', 'password')}@{os.getenv('DATABASE_HOST', '127.0.0.1')}:{os.getenv('DATABASE_PORT', '5432')}/{os.getenv('DATABASE_NAME', 'polls')}",
         conn_max_age=600,
     )
 }
